81cc691ddaca2efe1ab6e825c443a782d7ce5698
Ankit Mahindrakar <79319090+whoisanki@users.noreply.github.com>
2026-07-10 18:37:01 +0530

    Add: design-hashset - submission-1

---
+++ Data Structures & Algorithms/design-hashset/submission-1.js
@@ -0,0 +1,42 @@
+class MyHashSet {
+    constructor() {
+        this.set = Array.from({ length: 1000000 });
+    }
+
+    /**
+     * @param {number} key
+     * @return {void}
+     */
+    add(key) {
+        if (!this.set.includes(key)) {
+            this.set.push(key)
+        }
+    }
+
+    /**
+     * @param {number} key
+     * @return {void}
+     */
+    remove(key) {
+        let i = this.set.indexOf(key)
+        if(i > -1){
+            this.set.splice(i,1)
+        }
+    }
+
+    /**
+     * @param {number} key
+     * @return {boolean}
+     */
+    contains(key) {
+       return this.set.includes(key);
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = new MyHashSet()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
