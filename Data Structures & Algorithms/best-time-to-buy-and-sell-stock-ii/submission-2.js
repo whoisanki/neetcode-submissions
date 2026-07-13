1d644e6764f7fa1483be0ffafa114cd7f5a36cb6
Ankit Mahindrakar <79319090+whoisanki@users.noreply.github.com>
2026-07-13 20:31:54 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-2

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-2.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let n = prices.length;
+        let min = 0; // index not val
+        let profit = 0;
+        let setArr = [];
+        let singleProfit = 0;
+
+        for (let i = 0; i < n; i++) {
+            if (prices[i] > prices[min]) {
+                let diff = prices[i] - prices[min];
+                profit = Math.max(profit, diff);
+            }
+            if (prices[i] < prices[min]) {
+                min = i;
+            }
+            if (i < n - 1 && prices[i] < prices[i + 1]) {
+                let diff = prices[i + 1] - prices[i];
+                singleProfit = singleProfit + diff;
+            }
+        }
+
+        return Math.max(singleProfit, profit);
+    }
+}
